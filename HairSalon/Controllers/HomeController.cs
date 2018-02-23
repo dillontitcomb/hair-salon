3cd2a78d990d12d15672447b9796d5e6d3fd9855
Dillon Titcomb <dillontitcomb@gmail.com>
2018-02-23 15:14:52 -0800

    Adds client form with dropdown of stylist options.

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -11,5 +11,36 @@ namespace HairSalon.Controllers
         {
           return View();
         }
+				[HttpGet("/stylists")]
+        public ActionResult Stylists()
+        {
+					List<Stylist> stylistList = Stylist.GetAll();
+          return View(stylistList);
+        }
+				[HttpGet("/stylists/{id}")]
+        public ActionResult StylistInfo(int id)
+        {
+					Stylist foundStylist = Stylist.Find(id);
+          return View(foundStylist);
+        }
+				[HttpGet("/stylists/add")]
+        public ActionResult StylistForm()
+        {
+          return View();
+        }
+				[HttpPost("/stylists")]
+        public ActionResult AddStylist()
+        {
+					Stylist newStylist = new Stylist(Request.Form["stylist-name"]);
+					newStylist.Save();
+					List<Stylist> stylistList = Stylist.GetAll();
+          return View("Stylists", stylistList);
+        }
+				[HttpGet("/clients/add")]
+        public ActionResult ClientForm()
+        {
+					List<Stylist> stylistList = Stylist.GetAll();
+          return View(stylistList);
+        }
     }
 }
